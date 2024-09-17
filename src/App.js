3b5805ab5965bfd6acf3aaeac30a024c97819198
YounesLAR2 <younes.larfa2@gmail.com>
2024-09-17 14:24:57 +0100

    test two hooks useState and UseEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
+  const [first, setfirst] = useState(1);
+  const [input, setinput] = useState();
+
+  useEffect(() => {
+    // fetch requests
+    document.title = input
+    console.log("re-render-once")
+    const incrementer = setInterval(()=>{
+      
+    },[])
+  }, [input]);
+
+  console.log("re-render");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="App">My App in state = {first}</h1>
+      <h2>{input}</h2>
+      <input type="text" onChange={(e)=> setinput(e.target.value) } />
+      <button onClick={() => setfirst(first + 1)}>increment</button>
+    </>
   );
 }
 
